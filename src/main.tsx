7c433baf69ce0c87ceb6f0e62c27adeab87e01e5
Chris <i_am_ai_inc@outlook.com>
2026-05-17 12:19:12 -0400

    Add carryout route

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+﻿import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import AppCommerce from "./App-Commerce";
+import AppCarryout from "./App-Carryout";
 import LaunchSelector from "./LaunchSelector";
 import "./index.css";
 
@@ -10,6 +11,10 @@ function Router() {
 
   if (path === "/transactional") {
     return <AppCommerce />;
+  }
+
+  if (path === "/carryout") {
+    return <AppCarryout />;
   }
 
   if (path === "/informational") {
